f7bd2d0cef71e886b0775203a404646c47565159
namig <namig.nurmamedov@yandex.ru>
2021-09-08 15:23:03 +0400

    Initial commit

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+echo "<h1>Php app skeleton!</h1>";
+
+// При использовании composer можно сделать так:
+// require './../vendor/autoload.php';
+// $kernel = new \App\Kernel();
+// $kernel->run();
